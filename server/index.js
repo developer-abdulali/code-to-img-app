9e076385daf510d65daaa72b428b6a523547d777
developer-abdulali <ab.ali.soomro@gmail.com>
2024-08-24 02:58:22 +0500

    completed backend

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { userRoute } from "./routes/userRoutes.js";
+import { residencyRoute } from "./routes/residencyRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -13,6 +15,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
+app.use("/api/user", userRoute);
+app.use("/api/residency", residencyRoute);
+
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send({ message: `Server is running on port ${PORT}` });
 });
